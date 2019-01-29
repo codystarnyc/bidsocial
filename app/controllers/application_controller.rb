3a1f14054426fa535135d7ebd3eeef4786bdecf4
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-29 15:44:30 -0500

    creating auction controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_app_environment
 
     protected
 
@@ -8,4 +14,40 @@ class ApplicationController < ActionController::Base
     #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password)} 
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :username, :admin, :fName, :lName)} 
     end
-end
+    def after_sign_in_path_for user
+        session[:forward_url] ? session.delete(:forward_url) : super
+      end
+  
+      def set_app_environment
+        @charity = Charity.last
+        @current_auction = Auction.order(:finish).last
+      end
+  
+       def clear_item_search
+         user_searching = params[:name_sort] || \
+                          params[:current_bid_sort] || \
+                          params[:bid_count_sort]
+         session[:search] = nil unless user_searching
+       end
+  
+      def require_admin
+        unless current_user.admin?
+          redirect_to :back,
+                      :alert => 'Only Charity Administrators Allowed.'
+        end
+      end
+  
+      def require_login
+        unless user_signed_in?
+          session[:forward_url] = item_url(@item)
+                   redirect_to new_user_session_path,
+                   :alert => 'Please sign in or sign up before continuing.'
+        end
+      end
+    
+    def app_setup_if_needed
+      unless Charity.any? 
+       redirect_to new_charity_path
+      end
+    end
+  end
