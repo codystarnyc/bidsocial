e9ecc74a038a56ad4dc42d13772e3101ee5b7c03
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-29 15:30:18 -0500

    created watch_list_items model

---
+++ db/migrate/20190129202731_create_watch_list_items.rb
@@ -0,0 +1,13 @@
+class CreateWatchListItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :watch_list_items do |t|
+      t.integer :item_id
+      t.integer :user_id
+      t.boolean :wants_email
+
+      t.timestamps
+    end
+    add_index :watch_list_items, :item_id
+    add_index :watch_list_items, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_201340) do
+ActiveRecord::Schema.define(version: 2019_01_29_202731) do
 
   create_table "auctions", force: :cascade do |t|
     t.integer "charity_id"
@@ -93,6 +93,16 @@ ActiveRecord::Schema.define(version: 2019_01_29_201340) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "watch_list_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "user_id"
+    t.boolean "wants_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_watch_list_items_on_item_id"
+    t.index ["user_id"], name: "index_watch_list_items_on_user_id"
   end
 
 end
