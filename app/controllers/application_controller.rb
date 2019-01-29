c53e259599d7949bf24826d3bf7a1a182bab75ad
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-29 13:08:30 -0500

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username, :admin, :fName, :lName)} 
+    #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password)} 
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :username, :admin, :fName, :lName)} 
+    end
 end
